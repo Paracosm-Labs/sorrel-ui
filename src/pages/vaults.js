85c53ddb3e5c26fca9742facbd71adf7b06605ae
CosmicAnon <wescosmic@gmail.com>
2023-03-21 14:39:20 +0100

    minor ui updates

--- src/pages/vaults.js
+++ src/pages/vaults.js
@@ -22,7 +22,7 @@ const Vaults = () => {
 
           <div class="row">
             <div class="col"></div>
-            <div class="col text-center">
+            <div class="col text-center d-none">
               <button class="btn btn-outline-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasVaultCreate" aria-controls="offcanvasVaultCreate"><i class="fa-solid fa-plus"></i>&nbsp;Create Vault</button>
             </div>
           <div class="col"></div>
@@ -37,7 +37,7 @@ const Vaults = () => {
 
 
     <div class="container vaults content">
-      <div class="row mt-n4">
+      <div class="row mt-n10">
         <div class="col"></div>
         <div class="col-md-8">
 

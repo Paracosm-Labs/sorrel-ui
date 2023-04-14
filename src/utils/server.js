dfcd4f0acf2bcac11e81561933d28b4b7154f1dd
CosmicAnon <wescosmic@gmail.com>
2023-04-14 10:21:47 +0200

    Update server.js

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,5 +1,5 @@
 // const serverURL = "http://localhost:8000";
-const serverURL = "http://66.11.118.108:8000";
+const serverURL = "https://fd20-79-98-113-15.ngrok-free.app";
 
 
 export default serverURL;

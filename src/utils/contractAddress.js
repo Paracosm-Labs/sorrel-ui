3847ee8295490a9d2b1eb65773cfafb8b4031e1c
CosmicAnon <wescosmic@gmail.com>
2023-08-03 13:33:57 +0545

    initial migrations to  new deployment

--- src/utils/contractAddress.js
+++ src/utils/contractAddress.js
@@ -1,16 +1,16 @@
-const gStableManagerContractAddress = "TZDofabgTUK43589ow9zD3LseNPmVYqk6g";
-const SwapUSDDContractAddress = "TRH2YqLLNM6ekvqvBa13wB9NeCzMw7n2zU";
-const VaultUSDDContractAddress = "TArnA3jJaZLgqstK953Ct3Dz4tSo93YZe3";
+const gStableManagerContractAddress = "TF6GzQVGWk5PKcidG7bCCmQHYVqRQDtHNw";
+const SwapUSDDContractAddress = "TAQYVNxzFB2jFSJCFhk7QqVebdP7c77cJk";
+const VaultUSDDContractAddress = "TFUJuP19G43S6qFSWoRoabmC2VvL1dFUPc";
 const USDDMarketContractAddress = "TQq9o4PahyoLociVzCnBMRRDdPZrNNkW1f";
 const USDTMarketContractAddress = "TDxevGBzrd7NQWH8T2reeAz63wPPMfRnKV";
 
 // const SwapGStableContractAddress = "TB2ZNssD5WvGa2HSLHiYwe4xbsqW7ywcHN";
 
-const DepositoryContractAddress = "TQoiUFedkHM2RiBNCbDCMBFwAf8HTX8qKc";
-const TransferComptrollerAddress = "TGWs2QiZsoi4B1muRepSgEzJjQHiZjHEY9";
-const ConvertComptrollerAddress = "TFuXJa7Hk47ny5Xa5W6ePRoK6GdXMcateh";
+const DepositoryContractAddress = "TUR4za3qj9bZed33hJc9oBQrwTEz8JMq36";
+const TransferComptrollerAddress = "TGWs2QiZsoi4B1muRepSgEzJjQHiZjHEY9"; //TOCHANGE
+const ConvertComptrollerAddress = "TFuXJa7Hk47ny5Xa5W6ePRoK6GdXMcateh"; //TOCHANGE
 
-const DepositoryOwnerAddress = "TCiJCtTBhGSw8mMYYts67vCXUjdoFLLuYw";
+const DepositoryOwnerAddress = "TR8CREGJkKzUX9jyVzSz8tqpuQBAo9NNhg";
 
 export {gStableManagerContractAddress, 
     SwapUSDDContractAddress, 
